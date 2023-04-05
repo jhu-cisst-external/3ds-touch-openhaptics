5cf4ccdc78d52fce2e9b17c4c9ee090c03af35b1
Anton Deguet <anton.deguet@jhu.edu>
2023-04-05 07:31:08 -0400

    Added script for 2022 drivers, work on Ubuntu 20.04

--- install-3ds-touch-drivers-2022.sh
+++ install-3ds-touch-drivers-2022.sh
@@ -8,14 +8,14 @@ CUR_DIR=`pwd`
 
 echo "--- Downloading package"
 mkdir -p tmp
-curl https://s3.amazonaws.com/dl.3dsystems.com/binaries/Sensable/Linux/TouchDriver2019_2_15_Linux.tar.xz --output tmp/TouchDriver2019_2_15_Linux.tar.xz
+curl https://s3.amazonaws.com/dl.3dsystems.com/binaries/Sensable/Linux/TouchDriver2022_04_04.tgz --output tmp/TouchDriver2022_04_04.tgz
 
 echo "--- Extracting package"
-cd tmp && tar xf TouchDriver2019_2_15_Linux.tar.xz
+cd tmp && tar xf TouchDriver2022_04_04.tgz
 
 echo "--- Installing"
-sudo cp TouchDriver2019_2_14/bin/Touch* /usr/bin
-sudo cp TouchDriver2019_2_14/usr/lib/libPhantomIOLib42.so /usr/lib
+sudo cp TouchDriver2022_04_04/bin/Touch* /usr/bin
+sudo cp TouchDriver2022_04_04/usr/lib/libPhantomIOLib42.so /usr/lib
 
 echo "--- Create and configure shared directory for configuration files"
 sudo mkdir -p /usr/share/3DSystems/config
